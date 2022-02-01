04c729e1af0dbf333bdb15050c75823f546e0116
Nicolas Levée <nlevee@users.noreply.github.com>
2022-02-01 12:20:19 +0100

    feat(outputs.tf): add name_servers for delegation

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "zone_id" {
   description = "ID of the deployed DNS zone."
   value       = google_dns_managed_zone.this.id
 }
+
+output "zone_name_servers" {
+  description = "Delegate your managed_zone to these virtual name servers."
+  value = google_dns_managed_zone.this.name_servers
+}
